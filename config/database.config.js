71fce704ba8a1750c62f6a4c9046bc7fba8b1f49
Masoom Ali <masoomali.peer@cigniti.com>
2024-04-02 12:07:39 +0530

    added mangoDB link
    added mangoDB link

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url:'mongodb+srv://<username>:<password>:@learning.eauwn.mongodb.net/ecommerce-api?retryWrites=true&w=majority'
+  url: "mongodb+srv://masoom117:yBWxjI03h8F4y9Xn@flystylecluster.sci8fpb.mongodb.net/?retryWrites=true&w=majority&appName=flyStyleCluster",
 };
